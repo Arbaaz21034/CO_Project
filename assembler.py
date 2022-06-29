69993414665226dcd88b738da2f5bf0c23194a4d
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-06-29 19:49:46 +0530

    Added instruction name check + more

--- assembler.py
+++ assembler.py
@@ -10,16 +10,39 @@ Project Authors:
 
 """
 
+
+
+
+
 class Instruction:
     # Initiate the Instruction object with the assembly instruction derived from stdin
-    def __init__(self,asmInstruction):
+    def __init__(self, asmInstruction, lineNumber):
         self.instruction = asmInstruction;
+        self.lineNumber = lineNumber;
 
-    def add(self):
-        pass
+    # This method checks the validity of the instruction name (i.e the first word of the instruction)
+    def checkInstructionName(self):
+        instructionName = self.instruction.split()[0];
+        validInstructionNames = ['var','add','sub','mov','ld','st','mul','div','rs','ls','xor','or',
+        'and','not','cmp','jmp','jlt','jgt','je','hlt'];
 
-    def printInstruction(self):
-        print(self.instruction);
+        # Seeking errors
+        if (instructionName.lower() not in validInstructionNames):
+            if (not instructionName.endswith(":")): # Checks whether the instruction name is a label or not
+                print(f"Typo in instruction name on line {self.lineNumber}");
+                exit();
+
+
+
+
+
+    # Solely for debugging purposes
+    # This method is triggered when print(instructionObject) is called
+    def __str__(self):
+        return f"Instruction: {self.instruction} | Line: {self.lineNumber}";
+
+
+
 
 
 
@@ -67,36 +90,45 @@ outputList = []  # List contains everything that needs to be outputted to STDOUT
 
 
 
-
+lineCount = 0; # Keep a track of the instruction's line number (Needed for ErrorGen)
 
 def main():
     while 1:
         try:
             inputLine = input().strip();
+            global lineCount;
+            lineCount += 1;
 
             # Don't add the instruction to the instructions list when the instruction is empty or a blank line
             if (len(inputLine) == 0):
                 continue;
 
-            instructionsList.append(inputLine);
+            currentInstruction = Instruction(inputLine,lineCount); # Create an Instruction object
+            instructionsList.append(currentInstruction);
 
         except EOFError:
             print("\nAssembly input terminated. Generating your machine code.\n");
             break;
 
-
+"""
         except Exception as e:
             print("An unknown error occured. The error message is given below.");
             print(e);
             break;
+"""
+
+
+def generateBinaries():
+    for instructionObject in instructionsList:
+        #print(instructionObject); # DO NOT uncomment when in production
+        instructionObject.checkInstructionName() # Checks the validity of the instruction's first work aka the instruction name
 
 
 
 def output():
+    for outputLine in outputList:
+        pass;
     
-    for instructionLine in instructionsList:
-        instruction = Instruction(instructionLine); # Create an Instruction Object
-        instruction.printInstruction();
 
 
 
@@ -104,8 +136,10 @@ def output():
 
 
 
-
-# Calling the main program function
+# Calling the main program function which will accept the input
 main()
-# Calling the program which will output binaries or errors
+# Generating the binaries (if possible) and getting them ready for the STDOUT
+generateBinaries()
+# Calling the program which will output the 16-bit binaries line by line
 output()
+
